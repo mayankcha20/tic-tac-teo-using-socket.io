013c63befc800ec89ee26e111ad457acbb7ff579
Mayankcha20 <mayankchandr1@gmail.com>
2024-05-05 18:25:16 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("https://tic-tac-toe-using-socket-io-backend.onrender.com", {
+    const newSocket = io(import.meta.env.BACKEND, {
       autoConnect: true,
     });
 
